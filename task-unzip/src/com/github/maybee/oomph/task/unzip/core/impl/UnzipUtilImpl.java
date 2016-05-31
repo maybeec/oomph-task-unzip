3e1827df51dd7f39e7b34b2de6870c876f56d158
Malte Brunnlieb <malte.brunnlieb@capgemini.com>
2016-05-31 21:43:24 +0200

    #1 fixed major parent-pom issues

--- task-unzip/src/com/github/maybee/oomph/task/unzip/core/impl/UnzipUtilImpl.java
+++ task-unzip/src/com/github/maybee/oomph/task/unzip/core/impl/UnzipUtilImpl.java
@@ -3,118 +3,99 @@
  */
 package com.github.maybee.oomph.task.unzip.core.impl;
 
-import com.github.maybee.oomph.task.unzip.core.SetupTaskLogger;
-import com.github.maybee.oomph.task.unzip.core.UnzipUtil;
+import java.io.File;
+//TODO import net.lingala.zip4j.ZipFile
+import java.util.zip.ZipFile;
 
 //TODO import net.lingala.zip4j.progress.ProgressMonitor;
 import javax.swing.ProgressMonitor;
 
-import java.io.File;
-//TODO import net.lingala.zip4j.ZipFile
-import java.util.zip.ZipFile;
+import com.github.maybee.oomph.task.unzip.core.SetupTaskLogger;
+import com.github.maybee.oomph.task.unzip.core.UnzipUtil;
 
 /**
  * @author sholzer
  *
  */
-public class UnzipUtilImpl extends UnzipUtil
-{
+public class UnzipUtilImpl extends UnzipUtil {
 
-  /*
-   * (non-Javadoc)
-   * @see unziptask.core.UnzipUtil#unzip(java.lang.String, java.lang.String)
-   */
-  @Override
-  public void unzip(String zipFile, String destDir)
-  {
-    SetupTaskLogger.getLogger().logInfo("Starting Unzip process");
-    SetupTaskLogger.getLogger().log("Archive: " + zipFile);
-    SetupTaskLogger.getLogger().log("Destination: " + destDir);
+    @Override
+    public void unzip(String zipFile, String destDir) {
+        SetupTaskLogger.getLogger().logInfo("Starting Unzip process");
+        SetupTaskLogger.getLogger().log("Archive: " + zipFile);
+        SetupTaskLogger.getLogger().log("Destination: " + destDir);
 
-    ZipFile zip;
-    try
-    {
-      zip = new ZipFile(new File(zipFile));
-    }
-    catch (Exception ex)
-    {
-      SetupTaskLogger.getLogger().logError(ex.getMessage());
-      return;
-    }
-    ProgressMonitor progress = zip.getProgressMonitor();
+        ZipFile zip;
+        try {
+            zip = new ZipFile(new File(zipFile));
+        } catch (Exception ex) {
+            SetupTaskLogger.getLogger().logError(ex.getMessage());
+            return;
+        }
+        ProgressMonitor progress = zip.getProgressMonitor();
 
-    UnzipThread unzipThread = new UnzipThread(zip, destDir);
-    unzipThread.run();
-    while (unzipThread.isAlive())
-    {
-      SetupTaskLogger.getLogger().log("unziped: " + progress.getPercentDone() + "%");
-      try
-      {
-        Thread.sleep(500);
-      }
-      catch (InterruptedException e)
-      {
-      }
+        UnzipThread unzipThread = new UnzipThread(zip, destDir);
+        unzipThread.run();
+        while (unzipThread.isAlive()) {
+            SetupTaskLogger.getLogger().log("unziped: " + progress.getPercentDone() + "%");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+            }
+        }
+        SetupTaskLogger.getLogger().logInfo("Unzipping done");
     }
-    SetupTaskLogger.getLogger().logInfo("Unzipping done");
-  }
 
 }
 
-class UnzipThread extends Thread
-{
+class UnzipThread extends Thread {
 
-  /**
-   * zip4j api
-   */
-  ZipFile zip;
+    /**
+     * zip4j api
+     */
+    ZipFile zip;
 
-  /**
-   * destination directory
-   */
-  String destination;
+    /**
+     * destination directory
+     */
+    String destination;
 
-  /**
-   * if something went wrong. Since I cannot throw exceptions through the runnable interface I used this
-   * work around
-   */
-  boolean error = false;
+    /**
+     * if something went wrong. Since I cannot throw exceptions through the runnable interface I used this
+     * work around
+     */
+    boolean error = false;
 
-  /**
-   * What went wrong
-   */
-  String errorMessage;
+    /**
+     * What went wrong
+     */
+    String errorMessage;
 
-  /**
-   * Constructor
-   * @param zip
-   *            the zip4j api obj
-   * @param destination
-   *            destination directory
-   * @author sholzer (Feb 10, 2016)
-   */
-  UnzipThread(ZipFile zip, String destination)
-  {
-    this.zip = zip;
-    this.destination = destination;
-  }
-
-  /**
-   * {@inheritDoc}
-   * @author sholzer (Feb 10, 2016)
-   */
-  @Override
-  public void run()
-  {
-    try
-    {
-      zip.extractAll(destination);
+    /**
+     * Constructor
+     * @param zip
+     *            the zip4j api obj
+     * @param destination
+     *            destination directory
+     * @author sholzer (Feb 10, 2016)
+     */
+    UnzipThread(ZipFile zip, String destination) {
+        this.zip = zip;
+        this.destination = destination;
     }
-    catch (Exception e)
-    {
-      error = true;
-      errorMessage = e.getMessage();
+
+    /**
+     * {@inheritDoc}
+     * @author sholzer (Feb 10, 2016)
+     */
+    @Override
+    public void run() {
+        try {
+            zip.extractAll(destination);
+        } catch (Exception e) {
+            error = true;
+            errorMessage = e.getMessage();
+        }
     }
-  }
 
 }
